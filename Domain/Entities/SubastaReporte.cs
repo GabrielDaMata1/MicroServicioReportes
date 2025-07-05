c0ecef6b4c631e27a443ab8675def402c7f197eb
Angelbg1707 <165971014+Angelbg1707@users.noreply.github.com>
2025-07-05 14:01:26 -0400

    ReporteSubastasRealizadas[ #1]: Reporte de subastas realizadas
    Se implementó la lógica necesaria para generar el reporte de las subastas realizadas

---
+++ Domain/Entities/SubastaReporte.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Value_Object;
+
+namespace Domain.Entities
+{
+    public class SubastaReporte
+    {
+        public Guid IdSubasta { get; set; }
+        public NombreSubastaVO NombreSubasta { get; set; }
+        public DescripcionSubastaVO DescripcionSubasta { get; set; }
+        public FechaInicioSubastaVO FechaInicioSubasta { get; set; }
+        public FechaFinSubastaVO FechaFinSubasta { get; set; }
+        public IncrementoMinimoSubastaVO IncrementoMinimoSubasta { get; set; }
+        public PrecioReservaSubastaVO PrecioReservaSubasta { get; set; }
+        public EstadoSubastaVO EstadoSubasta { get; set; }
+        public Guid IdUsuario { get; set; }
+        public Guid IdProducto { get; set; }
+        public NombreProductoVO NombreProducto { get; set; }
+        public DescripcionProductoVO DescripcionProducto { get; set; }
+        public ImagenURLProductoVO ImagenURLProducto { get; set; }
+
+        public PrecioBaseProductoVO PrecioBaseProducto { get; set; }
+
+        public CategoriaProductoVO CategoriaProducto { get; set; }
+
+        public EstadoProductoVO EstadoProducto { get; set; }
+
+        public SubastaReporte(Guid idSubasta, NombreSubastaVO nombreSubasta, DescripcionSubastaVO descripcionSubasta, FechaInicioSubastaVO fechaInicioSubasta, 
+            FechaFinSubastaVO fechaFinSubasta, IncrementoMinimoSubastaVO incrementoMinimoSubasta, PrecioReservaSubastaVO precioReservaSubasta, EstadoSubastaVO estadoSubasta, Guid idUsuario,
+            Guid idProducto, NombreProductoVO nombreProducto, DescripcionProductoVO descripcionProducto, ImagenURLProductoVO imagenURLProducto, PrecioBaseProductoVO precioBaseProducto, 
+            CategoriaProductoVO categoriaProducto, EstadoProductoVO estadoProducto)
+        {
+            IdSubasta = idSubasta;
+            this.NombreSubasta = nombreSubasta;
+            this.DescripcionSubasta = descripcionSubasta;
+            this.FechaInicioSubasta = fechaInicioSubasta;
+            this.FechaFinSubasta = fechaFinSubasta;
+            this.IncrementoMinimoSubasta = incrementoMinimoSubasta;
+            this.PrecioReservaSubasta = precioReservaSubasta;
+            this.EstadoSubasta = estadoSubasta;
+            IdUsuario = idUsuario;
+            IdProducto = idProducto;
+            NombreProducto = nombreProducto;
+            DescripcionProducto = descripcionProducto;
+            ImagenURLProducto = imagenURLProducto;
+            PrecioBaseProducto = precioBaseProducto;
+            CategoriaProducto = categoriaProducto;
+            EstadoProducto = estadoProducto;
+        }
+    }
+
+
+}
